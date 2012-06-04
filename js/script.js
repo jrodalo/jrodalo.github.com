40bf95d59e8b9eb8acb37d532af5d78818e58771
jrodalo <jrodalo@gmail.com>
2012-06-04 22:16:57 +0100

    Modificado para que el canvas se cargue lo antes posible (sin esperar al onload)

--- js/script.js
+++ js/script.js
@@ -2,26 +2,19 @@
  * jrodalo.github.com by @jrodalo
  */
 
-window.onload = function() {
-
-	var canvas = document.createElement("canvas");
-
-	if (canvas && canvas.getContext) {
-
-		Pizarra.iniciar(canvas);
-
-	}
-
-};
-
 var Pizarra = {
 
-	config: { 
-		ancho: window.innerWidth, 
-		alto: window.innerHeight
+	config: {
+		ancho: window.innerWidth,
+		alto: window.innerHeight,
+		color: "#DDD"
 	},
 
-	iniciar: function(canvas) {
+	iniciar: function() {
+
+		var canvas = document.createElement("canvas");
+
+		if ( ! canvas || ! canvas.getContext) { return; }
 
 		var capaPizarra = document.createElement("div"),
 			context = canvas.getContext("2d");
@@ -29,7 +22,7 @@ var Pizarra = {
 		canvas.setAttribute("width", Pizarra.config.ancho);
 		canvas.setAttribute("height", Pizarra.config.alto);
 
-		capaPizarra.id = "pizarra";
+		capaPizarra.className = "pizarra";
 		capaPizarra.style.marginLeft = "-" + (Pizarra.config.ancho / 2) + "px";
 		capaPizarra.style.marginTop = "-" + (Pizarra.config.alto / 2) + "px";
 		capaPizarra.appendChild(canvas);
@@ -42,27 +35,29 @@ var Pizarra = {
 
 		var mousePos = Pizarra.getPosicionCursor(canvas, evt);
 
-	    context.beginPath();
+		context.beginPath();
 		context.moveTo(Pizarra.config.ancho / 2, Pizarra.config.alto / 2);
 		context.lineTo(mousePos.x, mousePos.y);
-		context.strokeStyle = "#DDD";
+		context.strokeStyle = Pizarra.config.color;
 		context.stroke();
 	},
 
 	getPosicionCursor: function(canvas, evt) {
 
-	    var obj = canvas, top = 0, left = 0;
+		var obj = canvas, top = 0, left = 0;
 
-	    while (obj && obj.tagName != 'BODY') {
-	        top += obj.offsetTop;
-	        left += obj.offsetLeft;
-	        obj = obj.offsetParent;
-	    }
+		while (obj && obj.tagName != 'BODY') {
+			top += obj.offsetTop;
+			left += obj.offsetLeft;
+			obj = obj.offsetParent;
+		}
 
-	    var mouseX = evt.clientX - left + window.pageXOffset;
-	    var mouseY = evt.clientY - top + window.pageYOffset;
+		var mouseX = evt.clientX - left + window.pageXOffset;
+		var mouseY = evt.clientY - top + window.pageYOffset;
 
-	    return { x: mouseX, y: mouseY };
+		return { x: mouseX, y: mouseY };
 	}
 
 };
+
+Pizarra.iniciar();

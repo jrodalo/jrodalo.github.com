84c3f0bc02f39cf4b22e1e7a0343f605c2568681
jrodalo <jrodalo@gmail.com>
2012-12-19 20:31:18 +0000

    Ya no es necesario calcular la posición del cursor en la capa porque está en 0,0 (top-left)

--- js/script.js
+++ js/script.js
@@ -31,29 +31,11 @@ var Pizarra = {
 
 	pintar: function(canvas, context, evt) {
 
-		var mousePos = Pizarra.getPosicionCursor(canvas, evt);
-
 		context.beginPath();
 		context.moveTo(Pizarra.config.ancho / 2, Pizarra.config.alto / 2);
-		context.lineTo(mousePos.x, mousePos.y);
+		context.lineTo(evt.clientX, evt.clientY);
 		context.strokeStyle = Pizarra.config.color;
 		context.stroke();
-	},
-
-	getPosicionCursor: function(canvas, evt) {
-
-		var obj = canvas, top = 0, left = 0;
-
-		while (obj && obj.tagName != 'BODY') {
-			top += obj.offsetTop;
-			left += obj.offsetLeft;
-			obj = obj.offsetParent;
-		}
-
-		var mouseX = evt.clientX - left + window.pageXOffset;
-		var mouseY = evt.clientY - top + window.pageYOffset;
-
-		return { x: mouseX, y: mouseY };
 	}
 
 };
